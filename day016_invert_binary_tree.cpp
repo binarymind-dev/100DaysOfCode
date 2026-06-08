2589ede3744b8bbe92d14e507f9feacf2b769224
Snehal Thombare <snehalthombare888@gmail.com>
2026-06-08 08:29:57 +0530

    Day 016: Invert Binary Tree - LeetCode Easy

---
+++ day016_invert_binary_tree.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) return nullptr;
+
+        swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
