7b0474897c6e4a8928cef76d5ed56d2f8da1f7a8
Binary Mind <snehalthombare888@gmail.com>
2026-06-28 06:59:18 +0530

    Day 034: Car Fleet - LeetCode Medium

--- day034_car_fleet.cpp
+++ day034_car_fleet.cpp
@@ -6,7 +6,7 @@ public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
         vector<pair<int,int>> cars(n);
-        
+    
         for (int i = 0; i < n; i++) {
             cars[i] = {position[i], speed[i]};
         }
